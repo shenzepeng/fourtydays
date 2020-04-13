6c7e0de40c6ad163ded1d5ba4ba4581aa1aedd55
mac <806871609@qq.com>
2020-04-13 13:54:59 +0800

    Initial commit

---
+++ src/main/java/com/example/fourtydays/FourtydaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.fourtydays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourtydaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourtydaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fourtydays/FourtydaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fourtydays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourtydaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
